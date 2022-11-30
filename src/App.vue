2d8863cba1aa512e995a74b1f7b9e22dd3a2d05f
qlq08 <103029755+qlq08@users.noreply.github.com>
2022-11-30 19:53:55 +0800

    首页路由和组件

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,13 @@
 <template>
-  <div class="container">
-    <!-- 修改数据,测试是否持久化 -->
-    App {{ $store.state.user.profile.account }}
-    <button @click="$store.commit('user/setUser', { account: 'quan' })">
-      设置用户信息
-    </button>
-  </div>
+  <!-- 一级路由 -->
+  <RouterView />
 </template>
 
 <script>
-// import { useStore } from 'vuex'
 export default {
   name: 'App'
-
 }
 </script>
 
-<style lang="less" scoped>
-</style>>
+<style>
+</style>
--- src/utils/request.js
+++ src/utils/request.js
@@ -6,20 +6,21 @@ import axios from 'axios'
 import store from '@/store'
 import router from '@/router'
 // 导出基准地址,原因:其他地方不是通过axios发请求的地方用上基准地址
-export const baseUrl = 'http://pcapi-xiaotuxian-front-devtest.itheima.net'
+export const baseURL = 'http://pcapi-xiaotuxian-front-devtest.itheima.net'
 const instrance = axios.create({
-  // axios 的一些配置,baseUrl timeout
-  baseUrl,
+  // axios 的一些配置,baseURL timeout
+  baseURL,
   timeout: 5000
 })
 // 配置请求拦截器
-instrance.interceptors.request.use((config) => {
+instrance.interceptors.request.use(config => {
   // 拦截业务逻辑
   // 进行请求配置的修改
   // 如果本地有token就在头部携带
   // 1.获取用户信息对象
   const { profile } = store.state.user
   // 2.判断是否有token
+  // console.log(profile.token)
   if (profile.token) {
     // 3.设置token
     config.headers.Authorization = `Bearer ${profile.token}`
